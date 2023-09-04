48f48d782f72abaad8590397bc949f4f7ce7d1ea
KymCat <kohina1452@gmail.com>
2023-09-04 17:11:59 +0900

    [백준] 1247번 부호 문제

---
+++ 문제/브론즈/B1247.py
@@ -0,0 +1,14 @@
+import sys
+
+for _ in range(3) :
+    n = int(input())
+    sum = 0
+    for _ in range(n) :
+        sum += int(sys.stdin.readline())
+
+    if sum == 0 :
+        print(0)
+    elif sum > 0 :
+        print('+')
+    else :
+        print('-')
