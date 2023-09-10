a8e2fcb06f6690d2a74285daf5e0e7880823bb15
KymCat <kohina1452@gmail.com>
2023-09-10 10:50:34 +0900

    [백준] 10845번 큐 문제

---
+++ 문제/실버/S10845.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+n = int(input())
+stack = deque()
+for _ in range(n) :
+    order = sys.stdin.readline().rstrip().split()
+    if len(order) > 1 :
+        stack.append(order[1])
+
+    else :
+        if order[0] == 'pop' :
+            print(-1 if len(stack) == 0 else stack.popleft())
+        elif order[0] == 'size' :
+            print(len(stack))
+        elif order[0] == 'empty' :
+            print( 1 if len(stack) == 0 else 0)
+        elif order[0] == 'front' :
+            print( -1 if len(stack) == 0 else stack[0])
+        else :
+            print(-1 if len(stack) == 0 else stack[len(stack)-1])
