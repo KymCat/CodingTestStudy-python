6fb8ec98ab92f032dc5c27910ab49984515c0aa4
KymCat <kohina1452@gmail.com>
2024-12-18 15:53:02 +0900

    정렬 : 퀵정렬

---
+++ 다시시작하는 코테공부/정렬/퀵정렬.py
@@ -0,0 +1,18 @@
+# 퀵정렬
+# 기준(pivot)을 정해서 큰 데이터와 작은 데이터의 위치를 교환
+# O(NlogN), 최악 : O(N^2)
+array = [7,5,9,0,3,1,6,2,4,8]
+
+def quick_sort(array) :
+    if len(array) <= 1 :
+        return array
+
+    pivot = array[0]
+    tail = array[1:] # 호어 분할
+
+    left_side = [ x for x in tail if x <= pivot ]
+    right_side = [ x for x in tail if x > pivot ]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
