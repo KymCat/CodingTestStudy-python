c2936cd40a37aa1da59e64a464471d884613d065
KymCat <kohina1452@gmail.com>
2025-01-27 15:50:36 +0900

    [백준] 11286번 절대값 힙 문제

--- 문제/실버/S11286.py
+++ 문제/실버/S11286.py
@@ -30,3 +30,18 @@ for _ in range(n) :
     else :
         heapq.heappush(q,x if x > 0 else -x)
         abs_dict[x] += 1
+
+
+# 훨씬 깔끔한 코드 - https://claude-u.tistory.com/154
+# n = int(input())
+# q = []
+#
+# for _ in range(n):
+#     x = int(sys.stdin.readline())
+#     if x != 0:
+#         heapq.heappush(q, (abs(x), x)) # 인덱스0의 값이 같다면 인덱스1로 다시 정렬함
+#     else:
+#         try:
+#             print(heapq.heappop(q)[1]) # (abs(x), x) 에서 x 값을 출력
+#         except:
+#             print(0)
